3231ac5ef0aae6de24d6bd9fc9ae6a4ffe0aa619
Kent Zysk <kent@kentzysk.com>
2023-02-11 01:22:18 -0600

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Initialize variable
+        String partyAffil = "AAA";
+        //Output asking for affiliation
+        System.out.println("What is your party affiliation? R for Republican, D for Democrat, I for Independent or Other");
+        if (partyAffil == "R") {
+            System.out.println("You get a Republican Elephant.");
+        } else if (partyAffil == "D") {
+            System.out.println("You get a Democratic Donkey.");
+        } else if (partyAffil == "I") {
+            System.out.println("You get an Independent Person.");
+        } else {
+            System.out.println("You get an Other Other.");
+        }
     }
 }
